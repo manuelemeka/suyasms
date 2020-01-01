7d2065772985312fcb8e141c4d4301dd27c8f850
xenophilya <manuelemeka@gmail.com>
2020-01-01 16:54:21 +0100

    update

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import {
 Platform,
@@ -11,12 +12,30 @@ export default class App extends Component {
 
   render() {
     return (
-   <view>
-     <view></view>
-     <view></view>
+   <view style={styles.container}>>
+     <view style={styles.banner}>></view>
+     <view style={styles.menu}>></view>
    </view>
     )
     }
     
     
 }
+
+const styles = StyleSheet.create({
+  container: {
+  // eslint-disable-next-line prettier/prettier
+  marginTop: 75,
+  alignItems: 'center',
+  flex: 1
+  },
+  banner: {
+  width: 50,
+  height: 50,
+  borderWidth: 2,
+  margin: 15,
+  alignItems: 'center',
+  justifyContent: 'center'
+  },
+  menu:{},
+  });
